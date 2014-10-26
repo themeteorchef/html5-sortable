432b958872c5b53d1a682eacad63798ae060dd4f
Ryan Glover <rglover@spengler.local>
2014-10-26 14:32:45 -0500

    Add html5 sortable library as submodule.

--- package.js
+++ package.js
@@ -1,17 +1,21 @@
 Package.describe({
   name: 'themeteorchef:html5-sortable',
-  summary: ' /* Fill me in! */ ',
-  version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  summary: 'HTML5 Sortable jQuery plugin by voidberg. Repackaged for Meteor.',
+  version: '0.1.6',
+  git: 'https://github.com/themeteorchef/html5-sortable.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('undefined');
-  api.addFiles('themeteorchef:html5-sortable.js');
+  api.versionsFrom('METEOR@0.9.4');
+  api.use('jquery');
+  api.addFiles([
+    'lib/html5-sortable/dist/html.sortable.min.js'
+  ],'client');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
+  api.use(['tinytest', 'test-helpers'], ['client', 'server']);
+  api.use('jquery', 'client');
   api.use('themeteorchef:html5-sortable');
   api.addFiles('themeteorchef:html5-sortable-tests.js');
 });
